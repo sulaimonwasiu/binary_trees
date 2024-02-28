1aa209fa1e527c260d5bacc1fe9a68ee2acfac5d
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-02-28 11:30:13 -0800

    Minor edit in pre-order traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,9 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (func == NULL && tree == NULL)
+	if (func == NULL)
+		return;
+	if (tree == NULL)
 		return;
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
