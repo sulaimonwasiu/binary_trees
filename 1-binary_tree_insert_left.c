a3219a5b35d72ec1099bf6bcebb824e2d08c7982
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-02-28 05:30:12 +0300

    Insert node to the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - insert node at the left side
+ * @parent: parent node
+ * @value: value of the node to insert
+ *
+ * Return: pointer to a binary tree
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		return (NULL);
+
+	binary_tree_t *new = binary_tree_node(parent, value);
+
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		parent->left->parent = new;
+	}
+
+	parent->left = new;
+	return (new);
+}
+
