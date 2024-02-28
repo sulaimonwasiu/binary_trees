8417ea12e257630f68fcf6c9b87f730a7bed5f61
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-02-28 10:44:36 -0800

    .

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,7 +15,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->n = value;
 	new->parent = parent;
@@ -31,4 +34,3 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	parent->left = new;
 	return (new);
 }
-
