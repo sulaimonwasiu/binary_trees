c023fb4768ae91bb5e9ed2454598a16299d11e42
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-02-29 13:07:26 +0300

    Size of a tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - get the size of a tree given a node
+ * @tree: pointer to a node in a tree
+ *
+ * Return: an integer(size of the tree)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_size = binary_tree_size(tree->left);
+	size_t right_size = binary_tree_size(tree->right);
+
+	return (1 + left_size + right_size);
+}
