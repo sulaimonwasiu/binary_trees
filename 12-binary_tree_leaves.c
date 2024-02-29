7b3a253ab68bd74aa18aefb423de03b20983ec4a
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-02-29 02:36:18 -0800

    Count of the leaves in  tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,6 +9,9 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t left_leaves, right_leaves;
 
+	if (tree == NULL)
+		return (0);
+
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
