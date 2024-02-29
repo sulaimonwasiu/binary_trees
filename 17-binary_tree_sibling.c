1f8f6cc408b21a500ef1b9164f10fcbf2baea76e
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-02-29 13:27:22 -0800

    Fix error

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent = node->parent;
+
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-
-	binary_tree_t *parent = node->parent;
 
 	if (parent->left == node)
 		return (parent->right);
