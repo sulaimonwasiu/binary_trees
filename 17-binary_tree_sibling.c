e1dc4f1f6ff385e2511f87bed518ba05e19c8e55
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-02-29 13:33:48 -0800

    Minor fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	binary_tree_t *parent = node->parent;
 
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL ||
+			parent->left == NULL || parent->right == NULL)
 		return (NULL);
 
 	if (parent->left == node)
